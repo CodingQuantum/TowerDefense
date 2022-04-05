fe3d69faf6176eae6609cd6e47bac0dc4b7a08c6
InformatikerGoat <lukas.hoerter@t-online.de>
2022-04-05 09:35:21 +0200

    Klassen geändert

--- src/KARTENAUSWAHL.java
+++ src/KARTENAUSWAHL.java
@@ -1,4 +1,45 @@
 
-public class KARTENAUSWAHL {
+public class KARTENAUSWAHL
+{
 
+	BILD hintergrund;
+	TASTER taster[];
+	STATISTIKEN statistiken;
+	//Attribut zurueck abklären
+	
+	
+	KARTENAUSWAHL()
+	{
+		
+	}
+	
+	void starten()
+	{
+		
+	}
+	
+	void zurueck()
+	{
+		
+	}
+	
+	void mousePressed()
+	{
+		
+	}
+	
+	void mouseReleased()
+	{
+		
+	}
+	
+	void mouseEntered()
+	{
+		
+	}
+	
+	void mouseExited()
+	{
+		
+	}
 }
--- src/STARTBILDSCHIRM.java
+++ src/STARTBILDSCHIRM.java
@@ -1,5 +1,44 @@
 
 public class STARTBILDSCHIRM 
 {
+	BILD bild;
+	//Attribut start Funktion Datentyp abkl‰ren
+	//Attribut schlieﬂen Funktion Datentyp abkl‰ren
+	STATISTIKEN statistiken;
+	
+	STARTBILDSCHIRM()
+	{
+		
+	}
+	
+	void verschwinden()
+	{
+		
+	}
+	
+	void schlieﬂen()
+	{
+		
+	}
+	
+	void mousePressed()
+	{
+		
+	}
+	
+	void mouseReleased()
+	{
+		
+	}
+	
+	void mouseEntered()
+	{
+		
+	}
+	
+	void mouseExited()
+	{
+		
+	}
 
 }
